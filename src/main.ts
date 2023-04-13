d17213c06ee221f9237f561675e7f668786d53fb
Johny Prieto <johnyprieto001@gmail.com>
2023-04-12 19:55:51 -0500

    create a pokemon inside database and validate it

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +10,13 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '2',
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
 
   await app.listen(3000);
 }
